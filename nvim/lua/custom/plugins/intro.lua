ecf72e26d8add30841946eafa890ca3d1e1acb24
jjxrry <114961129+jjxrry@users.noreply.github.com>
2024-10-17 11:24:24 -0700

    add split

--- nvim/lua/custom/plugins/intro.lua
+++ nvim/lua/custom/plugins/intro.lua
@@ -8,40 +8,6 @@ vim.opt.termguicolors = true -- Enable true color
 if vim.api.nvim_exec('echo argc()', true) == '0' then
   math.randomseed(os.time()) -- For random header.
 
-  -- To split our quote, artist and source.
-  -- And automatically center it for screen loader of the header.
-  local function split(s)
-    local t = {}
-    local max_line_length = vim.api.nvim_get_option 'columns'
-    local longest = 0 -- Value of longest string is 0 by default
-    for far in s:gmatch '[^\r\n]+' do
-      -- Break the line if it's actually bigger than terminal columns
-      local line
-      far:gsub('(%s*)(%S+)', function(spc, word)
-        if not line or #line + #spc + #word > max_line_length then
-          table.insert(t, line)
-          line = word
-        else
-          line = line .. spc .. word
-          longest = max_line_length
-        end
-      end)
-      -- Get the string that is the longest
-      if #line > longest then
-        longest = #line
-      end
-      table.insert(t, line)
-    end
-    -- Center all strings by the longest
-    for i = 1, #t do
-      local space = longest - #t[i]
-      local left = math.floor(space / 2)
-      local right = space - left
-      t[i] = string.rep(' ', left) .. t[i] .. string.rep(' ', right)
-    end
-    return t
-  end
-
   -- Function to retrieve console output.
   local function capture(cmd)
     local handle = assert(io.popen(cmd, 'r'))
@@ -121,41 +87,6 @@ if vim.api.nvim_exec('echo argc()', true) == '0' then
       '⠀⠀⠀⠀⠛⣿⣿⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⡾⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡿⠁',
       '⠀⠀⠀⠀⢐⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⠟⠀⠀⠀',
     },
-    -- {
-    --   '⠀⠀⠀⠀⠀⠀⢀⣄⢀⣴⡟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣦⠀⠱⣄⠀⠀⠀⠀',
-    --   '⠀⠀⠀⠀⠀⠀⣾⠈⠉⠀⠻⡷⠀⠀⣠⠖⠚⠉⠉⠉⠉⠓⢦⡀⠀⠀⠀⠀⠀⡤⠚⠉⠉⠉⠉⠑⠢⣄⠀⠈⢧⠀⠈⢳⡀⠀⠀',
-    --   '⠀⠀⠀⠀⠀⢰⠇⠀⠀⠀⠀⡇⢠⡞⠀⠀⠀⠀⢀⡀⠀⠀⠀⢹⠀⠀⠀⠀⢸⠁⠀⠀⡤⠀⠀⠀⠀⠈⣇⠀⠸⡆⠀⠀⢻⡄⠀',
-    --   '⠀⠀⠀⠀⢠⡏⠀⠀⠀⠀⠀⡇⠸⡇⠀⠀⠀⠀⠀⠀⠀⠀⢀⡞⠀⠀⠀⠀⠈⠣⣀⠀⠀⠀⠀⠀⠀⣠⠇⠀⠀⡇⠀⠀⠀⢿⠀',
-    --   '⠀⠀⠀⢀⡞⠀⠀⠀⠀⠀⠀⡇⠀⠙⠲⢤⣀⣀⣀⣀⠤⠖⠋⠀⣀⡴⠚⠓⠢⣄⡈⠙⠒⠒⠒⠒⠋⠁⠀⠀⢸⠃⠀⠀⠀⢸⡆',
-    --   '⠀⠀⠀⣼⠁⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⠴⠚⢥⡀⠀⠀⠀⡀⠙⠓⠒⠶⠦⢤⡄⠀⠀⣠⡏⠀⠀⠀⠀⢸⡇',
-    --   '⠀⠀⢰⡇⠀⠀⠀⠀⠀⠀⠀⢻⠀⠀⢀⣤⠶⠒⠚⠋⠉⠁⠀⠀⠀⠁⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⣇⢀⡴⠋⠀⠀⠀⠀⠀⣸⠃',
-    --   '⠀⠀⢸⠃⠀⠀⠀⠀⠀⠀⠀⢸⣧⡀⠀⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠻⣄⠀⠀⠀⠀⠀⢀⡟⠀',
-    --   '⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠘⡏⠉⠳⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠳⡄⠀⠀⢀⡾⠁⠀',
-    --   '⠀⠀⢸⡀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡀⣠⠞⠀⠀⠀',
-    -- },
-    -- {
-    --   '⠀⠀⠀⠀⠀⠀⣠⠞⠁⠀⠀⠀⢀⠀⠀⠀⠀⠀⠀⣾⣿⣿⣦⣀⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⣿⠀⠀⠀⠀⢀⡙⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀',
-    --   '⠀⠀⠀⠀⠀⠺⠓⠊⠉⠉⣲⠀⠈⡇⠀⠀⠀⠀⠀⣿⣿⡉⠉⠉⠑⠦⣳⡄⠀⠀⠉⠉⠉⠉⠛⠻⠿⠿⣿⣿⣿⣿⡄⠀⠀⠀⣿⠀⠀⠀⠀⠀⢻⠲⠿⣆⡀⠀⠀⠀⠀⠀⠀',
-    --   '⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠏⠀⣴⣿⡄⠀⠀⠀⢀⡏⠛⠃⠀⠀⠀⠀⠀⠙⠀⣠⡤⣤⣀⣠⠦⠀⠀⠀⠀⠀⣍⠻⣿⣄⠀⠀⢻⠀⠀⠀⠀⠀⠀⢇⠀⠀⠀⠀⠀⠀⠀⠀⠀',
-    --   '⠀⠀⠀⠀⠀⠀⠀⠀⠀⡸⠀⣼⣿⣿⣷⡀⠀⠀⢸⣇⠀⠀⠀⠀⠠⠄⠀⣰⡟⢉⣤⣤⡉⠁⠀⠀⠀⠀⠤⠀⠙⠁⢹⣿⣆⠀⢸⣧⡀⠀⠀⠀⠀⠸⡆⠀⠀⠀⠀⠀⠀⠀⠀',
-    --   '⠀⠀⠀⠀⠀⠀⠀⠀⢰⡇⣴⣿⣿⣿⣿⣧⡀⠀⢘⡟⠁⠀⠀⠀⠀⢀⡾⢻⡄⠸⣤⣌⣿⠀⠀⠀⠀⡄⠀⠀⣤⠀⣿⣿⣿⣆⢸⣿⣿⣦⡀⠀⠀⠀⢹⠀⠀⠀⠀⠀⠀⠀⠀',
-    --   '⠀⠀⠀⠀⠀⠀⠀⠀⣾⡜⢹⣿⣿⣿⣿⣿⣷⡀⠐⣧⡀⠀⢀⣴⠀⠈⠙⠲⠿⠶⠴⠾⠋⠀⠀⠐⠺⠆⠀⠀⠈⠀⣿⣿⣿⣿⣾⣿⣿⣿⣟⢦⡀⠀⠀⣇⠀⠀⠀⠀⠀⠀⠀',
-    --   '⠀⠀⠀⠀⠀⠀⠀⠀⠟⠁⢸⣿⣿⣿⣿⣿⣿⣷⡄⣟⣁⣀⣀⣠⣤⣤⣤⣤⣤⣄⣀⣀⣀⣀⣀⣀⠀⠀⠀⣴⡖⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⢹⢦⡀⢸⡄⠀⠀⠀⠀⠀⠀',
-    --   '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⢦⣸⠀⠙⢦⣧⠀⠀⠀⠀⠀⠀',
-    --   '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢼⣿⣿⡿⢻⡏⠉⠉⠈⠙⠲⢬⣗⡦⣄⠀⠀⠀⣠⠄⠀⠀⣹⠉⠉⢉⣟⣿⠟⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣆⣀⣠⣽⣦⣤⣤⣴⣶⣶',
-    --   '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⡛⡄⠀⣸⠀⠀⣴⠞⢻⡽⢿⣾⣍⢾⣿⣦⠞⠁⠀⠀⠀⢿⣤⣶⡾⢛⣾⣿⣷⠶⣄⡀⠈⠉⢹⠙⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿',
-    --   '⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣇⣷⣿⠀⣿⠀⠀⢿⡄⢿⡴⢆⡷⠹⣎⢻⠅⠀⠀⠀⠀⢀⣴⣿⠏⣴⠏⣾⢰⠎⣳⢀⣿⠂⠀⢸⠀⠀⣾⣹⡟⢹⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀',
-    --   '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⡏⢱⣿⡇⡟⠀⠀⠈⠳⢤⣉⣉⣤⡤⠟⠀⠀⠀⠀⠀⠀⣼⡿⠁⠀⠻⣄⣉⣓⣚⣣⡾⠃⠀⠀⢸⠀⢸⣿⠻⠃⡟⢿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀',
-    --   '⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣇⣿⡿⢻⡇⠀⠀⠀⠀⣀⣀⡤⠀⠀⠀⠀⠀⠀⠀⠀⢀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⢰⢿⣿⡀⣰⠃⠈⢻⣿⣷⡀⠀⠀⠀⠀⠀⠀',
-    --   '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⢿⣷⡈⠃⣠⠤⠒⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⢠⡄⠰⢤⣀⠀⠀⠀⠀⢸⣿⣿⢟⣵⠏⠀⠀⠸⣿⣿⣷⠀⠀⠀⠀⠀⠀',
-    --   '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣦⠉⠙⣇⠀⠀⠀⠀⢀⣤⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢲⢤⡀⠀⠌⠙⠲⢤⡀⡼⠋⠁⣹⠋⠀⠀⠀⠀⢻⣿⣿⣄⡀⠀⣀⣀⣀',
-    --   '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢦⠀⣿⠀⢀⡤⠞⠉⠀⠀⠀⠀⠀⠀⢀⣤⣀⢀⣀⣀⡀⠀⠀⠀⠈⠀⠈⠳⢆⡀⠀⠀⢙⣧⣀⡞⡇⠀⠀⠀⠀⠀⠈⢿⣿⣿⣿⣿⣿⡿⣿',
-    --   '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣈⣻⣹⣦⣉⣤⣤⣤⣾⣄⠀⠀⠀⠀⠈⠻⣽⣵⣟⡿⠃⠀⠀⠀⠀⠀⢧⡀⠀⠙⢦⣠⠟⢀⡿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⠀⠀',
-    --   '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⡇⠀⠀⠀⠀⠀⠈⠛⠋⡀⢀⣀⠀⢤⡀⠀⠀⠙⢆⠀⢠⡟⠀⣸⡁⢸⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣿⣿⣦⠀',
-    --   '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⠲⠶⣶⣉⣉⣉⣉⣉⢉⣉⣭⠭⠟⠛⠀⠀⠀⠈⢳⡟⣶⠋⢸⣿⣿⣿⣿⣶⣶⣤⠀⠀⠀⠀⠀⠀⠈⠻⢿⡄',
-    --   '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⣰⡏⣹⣿⣧⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀',
-    --   '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣧⡉⡳⢦⣀⣀⠀⠀⠀⠀⣀⣠⣴⣾⢟⣵⣿⣿⣺⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠃⠀⠀⠀⠀⠀⠀⠀ ',
-    -- },
     {
       '⠀⣼⣿⣿⣿⣿⣿⣿⡟⢸⣿⡇⠈⣿⣿⣿⠀⢧⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀',
       '⢀⣿⣿⣿⣿⣿⡿⣿⣷⠸⣿⡇⠀⠹⣿⣿⣇⢈⣧⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀',
@@ -176,6 +107,27 @@ if vim.api.nvim_exec('echo argc()', true) == '0' then
     },
   }
 
+  local function split(quote)
+    local q, a
+    if quote:sub(1, 1) == '"' then
+      -- Quote is wrapped in double quotes
+      q, a = quote:match '^"(.+)"%s+(.+)$'
+    else
+      -- Quote is not wrapped in quotes
+      q, a = quote:match '^(.*)%.%s+(.*)$'
+    end
+
+    -- Optional: Trim any leading/trailing whitespace
+    if q then
+      q = q:match '^%s*(.-)%s*$'
+    end
+    if a then
+      a = a:match '^%s*(.-)%s*$'
+    end
+
+    return q, a
+  end
+
   --
   -- Sections for Alpha.
   --
@@ -192,42 +144,41 @@ if vim.api.nvim_exec('echo argc()', true) == '0' then
   }
 
   local quotes = {
-    'Sometimes the right path is not the easiest one.',
-    'In the silence, we find our true selves.',
-    'The shadows hold secrets we dare not uncover.',
-    'Every ending is just a new beginning in disguise.',
-    'Time bends, but memories remain unbroken.',
-    'Embrace the chaos to find your own order.',
-    'The stars whisper truths we cannot see.',
-    'Lost souls wander in search of purpose.',
-    'Dreams are the echoes of our deepest desires.',
-    'Through darkness, light finds its way.',
-    'Choices define the essence of our being.',
-    'The unseen battles shape our destiny.',
-    'Whispers of the past guide our future.',
-    'In solitude, we discover our strength.',
-    'Fragile hearts mend in unexpected ways.',
-    'Echoes of silence speak louder than words.',
-    'The journey inward reveals outer truths.',
-    'Mysteries linger where shadows fall.',
-    'Hope flickers in the darkest nights.',
-    'Bound by fate, yet yearning for freedom.',
-    'Silent tears tell the loudest stories.',
-    'Invisible threads connect our souls.',
-    "In every sorrow, there's a hidden joy.",
-    'The unseen world dances with our reality.',
-    'Through the veil of time, truths emerge.',
-    'Whispers of the wind carry forgotten tales.',
-    'Beneath the surface lies untold beauty.',
-    'In the depths, we find our light.',
-    'The heart sees what the eyes cannot.',
-    'Silent echoes resonate through eternity.',
-    'Only cunies thrive in the hours of darkness. -Lindsey',
+    'Only cunies thrive in the hours of darkness. Lindsey',
+    '"We are what we repeatedly do. Excellence, then, is not an act, but a habit." Aristotle',
+    '"To be is to be perceived." George Berkeley',
+    '"The mind is furnished with ideas by experience alone." John Locke',
+    '"All things excellent are as difficult as they are rare." Baruch Spinoza',
+    '"Freedom is the recognition of necessity." Friedrich Engels',
+    '"Man is condemned to be free; because once thrown into the world, he is responsible for everything he does." Jean-Paul Sartre',
+    '"Life must be understood backward. But it must be lived forward." Søren Kierkegaard',
+    '"Happiness is not an ideal of reason but of imagination." Immanuel Kant',
+    '"The unexamined life is not worth living." Socrates',
+    '"Every man takes the limits of his own field of vision for the limits of the world." Arthur Schopenhauer',
+    '"One must still have chaos in oneself to be able to give birth to a dancing star." Friedrich Nietzsche',
+    '"The more unintelligent a man is, the less mysterious existence seems to him." Arthur Schopenhauer',
+    '"The only way to deal with this life is to find meaning in the chaos." Søren Kierkegaard',
+    '"A man is but the product of his thoughts; what he thinks, he becomes." Mahatma Gandhi',
+    '"He who knows others is wise; he who knows himself is enlightened." Lao Tzu',
+    '"The desire for safety stands against every great and noble enterprise." Tacitus',
+    '"To fear death, my friends, is only to think ourselves wise without being wise." Socrates',
+    '"The man who has a why to live can bear almost any how." Friedrich Nietzsche',
+    '"Time is the moving image of eternity." Plato',
+    '"The people who are crazy enough to think they can change the world are the ones who do." Steve Jobs',
+    '"I do not think you can name many great inventions that have been made by married men." Nikola Tesla',
+    '"It’s not a faith in technology. It’s faith in people." Steve Jobs',
+    '"The present is theirs; the future, for which I really worked, is mine." Nikola Tesla',
+    '"Innovation distinguishes between a leader and a follower." Steve Jobs',
+    '"The scientist discovers a new type of material or energy and the engineer discovers a new use for it." Gordon Moore',
+    '"Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work." Steve Jobs',
+    '"If you want to find the secrets of the universe, think in terms of energy, frequency, and vibration." Nikola Tesla',
+    '"The advance of technology is based on making it fit in so that you don’t really even notice it, so it’s part of everyday life." Bill Gates',
+    '"Any sufficiently advanced technology is indistinguishable from magic." Arthur C. Clarke',
   }
 
   local function get_random_quote()
     local quote = quotes[math.random(#quotes)]
-    return quote
+    return split(quote)
   end
 
   local footer = {
