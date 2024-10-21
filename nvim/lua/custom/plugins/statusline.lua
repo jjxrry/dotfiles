53ba2ed78bb89d608cb8aec41cf586277cb85ffa
jjxrry <114961129+jjxrry@users.noreply.github.com>
2024-10-21 12:57:31 -0700

    update custom pwd for lualina

--- nvim/lua/custom/plugins/statusline.lua
+++ nvim/lua/custom/plugins/statusline.lua
@@ -1,9 +1,16 @@
+local function pwd()
+  return vim.fn.getcwd()
+end
+
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   opts = {
     options = {
       theme = 'edge',
+    },
+    sections = {
+      lualine_c = { pwd },
     },
   },
 }
